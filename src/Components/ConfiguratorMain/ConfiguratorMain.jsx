044c75909a58fa39a18d3932e7deb090b8a9f938
IhorVerba <ihor.verbivskyi@gmail.com>
2024-01-22 17:22:35 +0200

    configurator

--- src/Components/ConfiguratorMain/ConfiguratorMain.jsx
+++ src/Components/ConfiguratorMain/ConfiguratorMain.jsx
@@ -3,24 +3,49 @@ import { useParams } from "react-router-dom";
 import { productsData } from "../../api/products";
 import { colors } from "../../api/products";
 import { useState } from "react";
-// import Golf from "../../assets/images/Configurator/golf-black-w16.png";
 
 export const ConfiguratorMain = () => {
   const { id } = useParams();
   const car = productsData.find((c) => c.id === parseInt(id));
 
-  const [state, setState] = useState({
-    name: car.name
-  })
+  const buildImagePath = (model, color, wheel) => {
+    return `/Configurator/${model[color.value][0][wheel.value].src}`;
+  };
 
+  const [configuration, setConfiguration] = useState({
+    selectedColor: car.color[0],
+    selectedWheel: car.wheels[0],
+    imagePath: buildImagePath(car.models[0], car.color[0], car.wheels[0]),
+  });
+  
+  
+  const handleColorChange = (color) => {
+    setConfiguration((prevConfig) => ({
+      ...prevConfig,
+      selectedColor: color,
+      imagePath: buildImagePath(car.models[0], color, prevConfig.selectedWheel),
+    }));
+  };
+  
+  const handleWheelChange = (wheel) => {
+    setConfiguration((prevConfig) => ({
+      ...prevConfig,
+      selectedWheel: wheel,
+      imagePath: buildImagePath(car.models[0], prevConfig.selectedColor, wheel),
+    }));
+  };
+  
+  const totalPrice =
+  car.price + configuration.selectedColor.price + configuration.selectedWheel.price;
+  
   return (
     <div className="ConfiguratorMain">
       <div className="ConfiguratorMain__container">
         <div className="ConfiguratorMain__imageContainer">
-          {/* <img src={Golf} alt="golf" /> */}
-          <img src={`/Configurator/${car.imgConfig}`} alt={`${car.name}`} />
-          {/* <span>Current name is {car.name}</span> */}
-          {/* <span>Current id is {car.id}</span> */}
+          {/* <img src={`/Configurator/${car.imgConfig}`} alt={`${car.name}`} /> */}
+          {/* <img src={`/Configurator/${selectedColor.imgConfig}`}alt={`${car.name}`} /> */}
+          {/* <img src={`/Configurator/${car.models[0].white[0].wheel_1.src}`} alt={`${car.name}`} /> */}
+          <img src={configuration.imagePath} alt={`${car.name}`} />
         </div>
         <div className="ConfiguratorMain__optionsContainer">
           <div className="ConfiguratorMain__optionsWrapper">
@@ -28,17 +53,21 @@ export const ConfiguratorMain = () => {
             <div className="ConfiguratorMain__options">
               <ul>
                 {colors.map((color, index) => (
-                  <li key={index}>
+                  <li
+                    key={index}
+                    onClick={() => handleColorChange(color)}
+                    className={color === configuration.selectedColor ? "selected" : ""}
+                  >
                     <img src={`/Configurator/${color.img}`} alt={color.name} />
                   </li>
                 ))}
               </ul>
             </div>
-              <div className="ConfiguratorMain__options--text">
-                {`COLOR NAME `}
-                <b>{`UAH COLOR PRICE`}</b>
-                <p></p>
-              </div>
+            <div className="ConfiguratorMain__options--text">
+              {`${configuration.selectedColor.label} `}
+              <b>{`UAH ${configuration.selectedColor.price}`}</b>
+              <p></p>
+            </div>
           </div>
 
           <div className="ConfiguratorMain__optionsWrapper">
@@ -46,7 +75,11 @@ export const ConfiguratorMain = () => {
             <div className="ConfiguratorMain__options">
               <ul>
                 {car.wheels.map((wheel, index) => (
-                  <li className="ConfiguratorMain__wheels" key={index}>
+                  <li
+                    key={index}
+                    onClick={() => handleWheelChange(wheel)}
+                    className={wheel === configuration.selectedWheel ? "selected" : ""}
+                  >
                     <img
                       src={`/Configurator/wheels/${wheel.src}`}
                       alt={wheel.label}
@@ -56,17 +89,17 @@ export const ConfiguratorMain = () => {
               </ul>
             </div>
             <div className="ConfiguratorMain__options--text">
-                {`Wheel NAME `}
-                <b>{`UAH WHEEL PRICE`}</b>
-                <p></p>
-              </div>
+              {`${configuration.selectedWheel.label} `}
+              <b>{`UAH ${configuration.selectedWheel.price}`}</b>
+              <p></p>
+            </div>
           </div>
         </div>
       </div>
       <div className="ConfiguratorMain__footer">
         <div>
           <span>
-            Загальна ціна: <b>UAH {car.price}</b>
+            Загальна ціна: <b>UAH {totalPrice}</b>
           </span>
         </div>
       </div>
--- src/api/products.js
+++ src/api/products.js
@@ -1,7 +1,17 @@
 export const colors = [
   { label: "Midnight Grey", value: "grey", price: 0, img: "color-grey.png" },
-  { label: "Pearl White", value: "white", price: 30000, img: "color-white.png" },
-  { label: "Solid Black", value: "black", price: 40000, img: "color-black.png" },
+  {
+    label: "Pearl White",
+    value: "white",
+    price: 30000,
+    img: "color-white.png",
+  },
+  {
+    label: "Solid Black",
+    value: "black",
+    price: 40000,
+    img: "color-black.png",
+  },
 ];
 
 export const productsData = [
@@ -9,11 +19,29 @@ export const productsData = [
     id: 1,
     name: "Volkswagen Golf",
     img: "golf-r-line.png",
-    imgConfig: 'golf-grey-w16.png',
     color: colors,
     price: 1100000,
     models: [
-      
+      {
+        grey: [
+          {
+            wheel_1: { src: "golf-grey-w16.png", value: "wheel_1" },
+            wheel_2: { src: "golf-grey-w17.png", value: "wheel_2" },
+          },
+        ],
+        white: [
+          {
+            wheel_1: { src: "golf-white-w16.png", value: "wheel_1" },
+            wheel_2: { src: "golf-white-w17.png", value: "wheel_2" },
+          },
+        ],
+        black: [
+          {
+            wheel_1: { src: "golf-black-w16.png", value: "wheel_1" },
+            wheel_2: { src: "golf-black-w17.png", value: "wheel_2" },
+          },
+        ],
+      },
     ],
     wheels: [
       {
@@ -30,84 +58,172 @@ export const productsData = [
       },
     ],
   },
+
   {
     id: 2,
     name: "Volkswagen T-Roc",
     img: "t-roc.png",
-    imgConfig: 'troc-grey-w16.png',
     color: colors,
+    models: [
+      {
+        grey: [
+          {
+            wheel_1: { src: "troc-grey-w16.png", value: "wheel_1" },
+            wheel_2: { src: "troc-grey-w17.png", value: "wheel_2" },
+          },
+        ],
+        white: [
+          {
+            wheel_1: { src: "troc-white-w16.png", value: "wheel_1" },
+            wheel_2: { src: "troc-white-w17.png", value: "wheel_2" },
+          },
+        ],
+        black: [
+          {
+            wheel_1: { src: "troc-black-w16.png", value: "wheel_1" },
+            wheel_2: { src: "troc-black-w17.png", value: "wheel_2" },
+          },
+        ],
+      },
+    ],
     price: 1200000,
     wheels: [
       {
         src: "troc/wheel16-troc.png",
         label: '16" Диски "Chester" R16',
-        value: "",
+        value: "wheel_1",
         price: 0,
       },
       {
         src: "troc/wheel17-troc.png",
         label: '17" Диски "Johannesburg" R17',
-        value: "",
+        value: "wheel_2",
         price: 40000,
       },
     ],
   },
+
   {
     id: 3,
     name: "Volkswagen Tiguan",
     img: "tiguan.png",
-    imgConfig: 'tiguan-grey-w17.png',
     color: colors,
     price: 1600000,
+    models: [
+      {
+        grey: [
+          {
+            wheel_1: { src: "tiguan-grey-w17.png", value: "wheel_1" },
+            wheel_2: { src: "tiguan-grey-w19.png", value: "wheel_2" },
+          },
+        ],
+        white: [
+          {
+            wheel_1: { src: "tiguan-white-w17.png", value: "wheel_1" },
+            wheel_2: { src: "tiguan-white-w19.png", value: "wheel_2" },
+          },
+        ],
+        black: [
+          {
+            wheel_1: { src: "tiguan-black-w17.png", value: "wheel_1" },
+            wheel_2: { src: "tiguan-black-w19.png", value: "wheel_2" },
+          },
+        ],
+      },
+    ],
     wheels: [
       {
         src: "tiguan/wheel17-tiguan.png",
         label: '17" Диски "Montana" R17',
-        value: "",
+        value: "wheel_1",
         price: 0,
       },
       {
         src: "tiguan/wheel19-tiguan.png",
         label: '19" Диски "Victoria Falls" R19',
-        value: "",
+        value: "wheel_2",
         price: 40000,
       },
     ],
   },
+
   {
     id: 4,
     name: "Volkswagen Arteon",
     img: "arteon-r-line.png",
-    imgConfig: 'arteon-grey-w18.jpg',
     color: colors,
     price: 2000000,
+    models: [
+      {
+        grey: [
+          {
+            wheel_1: { src: "arteon-grey-w18.jpg", value: "wheel_1" },
+            wheel_2: { src: "arteon-grey-w20.jpg", value: "wheel_2" },
+          },
+        ],
+        white: [
+          {
+            wheel_1: { src: "arteon-white-w18.jpg", value: "wheel_1" },
+            wheel_2: { src: "arteon-white-w20.jpg", value: "wheel_2" },
+          },
+        ],
+        black: [
+          {
+            wheel_1: { src: "arteon-black-w18.jpg", value: "wheel_1" },
+            wheel_2: { src: "arteon-black-w20.jpg", value: "wheel_2" },
+          },
+        ],
+      },
+    ],
     wheels: [
       {
         src: "arteon/wheel18-arteon.jpg",
         label: '18" Диски "Almere Adamantium" R18',
-        value: "",
+        value: "wheel_1",
         price: 0,
       },
       {
         src: "arteon/wheel20-arteon.jpg",
         label: '20" Диски "Nashville" R20',
-        value: "",
+        value: "wheel_2",
         price: 40000,
       },
     ],
   },
+
   {
     id: 5,
     name: "Volkswagen Touareg",
     img: "touareg.png",
-    imgConfig: 'touareg-grey-w20.png',
     color: colors,
     price: 3000000,
+    models: [
+      {
+        grey: [
+          {
+            wheel_1: { src: "touareg-grey-w20.png", value: "wheel_1" },
+            wheel_2: { src: "touareg-grey-w21.png", value: "wheel_2" },
+          },
+        ],
+        white: [
+          {
+            wheel_1: { src: "touareg-white-w20.png", value: "wheel_1" },
+            wheel_2: { src: "touareg-white-w21.png", value: "wheel_2" },
+          },
+        ],
+        black: [
+          {
+            wheel_1: { src: "touareg-black-w20.png", value: "wheel_1" },
+            wheel_2: { src: "touareg-black-w21.png", value: "wheel_2" },
+          },
+        ],
+      },
+    ],
     wheels: [
       {
         src: "touareg/wheel20-touareg.png",
         label: '20" Диски "Braga" R20',
-        value: "",
+        value: "wheel_1",
         price: 0,
       },
       {
@@ -119,13 +235,3 @@ export const productsData = [
     ],
   },
 ];
-
-
-export const initialConfig = {
-  1: {
-    car: "long_range_plus",
-    model: "",
-    color: "white",
-    wheels: "",
-  }
-};
